9fdf69d8244ba35f401f8bc798db4da4b377f805
12leticiasilva0990 <130669115+12leticiasilva0990@users.noreply.github.com>
2023-06-01 08:50:12 -0300

    commit lety

--- mediabi.php
+++ mediabi.php
@@ -1,7 +1,12 @@
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Calculadora de Média</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora de Media</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>Calculadora de Média</h1>
@@ -32,7 +37,7 @@
         
     }
     ?>
-    
+
     <form method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
         <label for="nota1">Nota 1:</label>
         <input type="number" name="nota1" step="0.1" required><br>
