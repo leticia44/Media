95241374f78f492d493f5fb0bd4113b91e4f59af
12leticiasilva0990 <130669115+12leticiasilva0990@users.noreply.github.com>
2023-06-01 08:42:50 -0300

    commit

--- mediabi.php
+++ mediabi.php
@@ -32,5 +32,21 @@
         
     }
     ?>
+    
+    <form method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+        <label for="nota1">Nota 1:</label>
+        <input type="number" name="nota1" step="0.1" required><br>
+
+        <label for="nota2">Nota 2:</label>
+        <input type="number" name="nota2" step="0.1" required><br>
+
+        <label for="nota3">Nota 3:</label>
+        <input type="number" name="nota3" step="0.1" required><br>
+
+        <label for="nota4">Nota 4:</label>
+        <input type="number" name="nota4" step="0.1" required><br>
+
+        <input type="submit" value="Calcular Média">
+    </form>
     </body>
 </html>
